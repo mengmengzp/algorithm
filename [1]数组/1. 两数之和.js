e9d12a95943b03ce90a3a551e135bc7061031148
lvmengmeng <lvmengmeng@baidu.com>
2022-04-05 10:22:51 +0800

    两数之和

---
+++ [1]数组/1. 两数之和.js
@@ -0,0 +1,38 @@
+/*
+ * [1] 两数之和
+ */
+
+// 题目链接： https://leetcode-cn.com/problems/two-sum/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// 方法一：暴力遍历（时间复杂度O（n^2））
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (a[i] + a[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+    return []
+}
+
+// 方法二: hash（O(n)）
+var twoSum = function(nums, target) {
+    // 定义一个对象存储当前项和对应的下标
+    let map = {};
+    for(let i = 0; i < nums.length; i++) {
+        let result = target - nums[i];
+        // 对象中是否包含目标值，没有进行存储
+        if (map.hasOwnProperty(result)) {
+            return [map[result], i];
+        }
+        map[nums[i]] = i;
+    }
+    return [];
+};
